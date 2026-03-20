ecf279d2a96596050e67599ae30dad2c71429c18
ouarainanas <ouarainanas@gmail.com>
2026-03-20 02:16:36 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ char check() {
         if (b[0][i] == b[1][i] && b[1][i] == b[2][i]) return b[0][i];
     }
     if (b[0][0] == b[1][1] && b[1][1] == b[2][2]) return b[0][0];
-    if (b[0][2] == b[1][1] && b[1][0] == b[2][0]) return b[0][2];
+    if (b[0][2] == b[1][1] && b[1][1] == b[2][0]) return b[0][2];
     return ' ';
 }
 bool full() {
